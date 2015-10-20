4b96727f9b31340482c8355f1cef22b2fe00576f
Michael Wood <michael.wood@optimizely.com>
2015-10-20 14:48:49 -0700

    add source_url

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email "michael.wood@optimizely.com"
 license          "MIT"
 description      "Installs/Configures opentsdb_handler"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+source_url       "https://github.com/optimizely/chef_opentsdb_handler"
 version          "0.1.0"
 
 recipe           "opentsdb_handler::default",              "Sets up OpenTSDB chef handler"
